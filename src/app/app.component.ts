ff6ca25fb726eb79a84f498fd9be28333c8001c5
Hepsox <marie.delaire@outlook.com>
2024-03-16 16:00:18 +0100

    router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from './menu/menu.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MenuComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
